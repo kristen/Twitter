d00449d479214fa4aec0d7dc250af06f0d4e1573
Kristen Sundquist <kssundquist@gmail.com>
2015-02-20 07:18:14 -0800

    Add pull to refresh

--- Twitter/ViewControllers/TweetsViewController.swift
+++ Twitter/ViewControllers/TweetsViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class TweetsViewController: UIViewController {
     private var tweets = [Tweet]()
     @IBOutlet private weak var tweetsTableView: UITableView!
+    private var refreshControl: UIRefreshControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,24 +20,35 @@ class TweetsViewController: UIViewController {
         
         tweetsTableView.dataSource = self
         
+        // UITableViewCell
         tweetsTableView.registerNib(UINib(nibName: "TweetCell", bundle: nil), forCellReuseIdentifier: "TweetCell")
         tweetsTableView.estimatedRowHeight = 95
         tweetsTableView.rowHeight = UITableViewAutomaticDimension
         
+        // UIRefreshControl
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "fetchTweets", forControlEvents: UIControlEvents.ValueChanged)
+        tweetsTableView.insertSubview(refreshControl, atIndex: 0)
         
         navigationItem.title = "Tweets"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: "onLogout")
         
+        fetchTweets()
+    }
+
+    
+    func fetchTweets() {
         TwitterClient.sharedInstance.homeTimelineWithParams(nil, completion: { (tweetsOptional, error) -> () in
             if let tweets = tweetsOptional {
                 self.tweets = tweets
                 self.tweetsTableView.reloadData()
+                self.refreshControl.endRefreshing()
             } else {
                 println("error loading tweets")
             }
         })
     }
-
+    
     func onLogout() {
         User.currentUser?.logout()
     }
